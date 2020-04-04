bf40a0eb2bdd33d5be757dd0c0fa2b2897565dfb
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2020-04-03 21:17:04 -0600

    added missing files

---
+++ EmigLog/EmigLog.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace EmigLog
+{
+    public class EmigLog
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var conn = factory.CreateConnection();
+            using var channel = conn.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var mess = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(mess);
+            channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+
+            Console.WriteLine($"[x] sent {mess}");
+
+            Console.WriteLine("Press [enter to exit]");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return  ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!!");
+        }
+    }
+}
---
+++ ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ReceiveLogs
+{
+    public class ReceiveLogs
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var conn = factory.CreateConnection();
+            using var channel = conn.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+            Console.WriteLine("[*] Waitin for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var mess = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"[x] {mess}");
+            };
+
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+            Console.WriteLine("press [enter] to exit");
+            Console.ReadLine();
+        }
+
+    }
+}
